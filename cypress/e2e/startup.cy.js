1b08ce92dfdb5d361a17d6c4e7277e07d1e0b23f
KrishanNandan <krishannandan09@gmail.com>
2023-11-24 14:57:19 +0530

    changes for cypress configuration

--- cypress/e2e/startup.cy.js
+++ cypress/e2e/startup.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000');
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000");
+  });
+  it("button click", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("span").contains("Good news: We're going to make better UI soon.");
+    cy.get("button").click();
+    cy.wait(5000);
+    cy.get("p").contains("Title");
+  });
+  it("negative testing button", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("button").should('be.visible');
+  });
+});
